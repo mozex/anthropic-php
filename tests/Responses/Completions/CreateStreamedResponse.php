2dc50e39f20a3e2b48ca6f528dbaf4f138afe7b7
Mozex <mozex.gm@gmail.com>
2024-05-01 13:46:44 +0300

    test for completions stream create response

--- tests/Responses/Completions/CreateStreamedResponse.php
+++ tests/Responses/Completions/CreateStreamedResponse.php
@@ -2,16 +2,44 @@
 
 use Anthropic\Responses\Completions\CreateStreamedResponse;
 
+test('from', function () {
+    $completion = CreateStreamedResponse::from(completion());
+
+    expect($completion)
+        ->toBeInstanceOf(CreateStreamedResponse::class)
+        ->type->toBe('completion')
+        ->id->toBe('compl_01Sb5nmX365bQaWJ3jDfSgqB')
+        ->completion->toBe(' Hello!')
+        ->stop_reason->toBe('stop_sequence')
+        ->model->toBe('claude-2.1')
+        ->stop->toBe('\n\nHuman:')
+        ->log_id->toBe('compl_01Sb5nmX365bQaWJ3jDfSgqB');
+});
+
+test('as array accessible', function () {
+    $completion = CreateStreamedResponse::from(completion());
+
+    expect($completion['id'])->toBe('compl_01Sb5nmX365bQaWJ3jDfSgqB');
+});
+
+test('to array', function () {
+    $completion = CreateStreamedResponse::from(completion());
+
+    expect($completion->toArray())
+        ->toBeArray()
+        ->toBe(completion());
+});
+
 test('fake', function () {
     $response = CreateStreamedResponse::fake();
 
     expect($response->getIterator()->current())
-        ->id->toBe('cmpl-6ynJi2uZZnKntnEZreDcjGyoPbVAn');
+        ->id->toBe('compl_01U8ZMthep4UrANhFshPMUFK');
 });
 
 test('fake with override', function () {
     $response = CreateStreamedResponse::fake(completionStream());
 
     expect($response->getIterator()->current())
-        ->id->toBe('cmpl-6wcyFqMKXiZffiydSfWHhjcgsf3KD');
+        ->id->toBe('compl_01GS5bBdxpspiwnHYoCVk9Di');
 });
