6536d6c04106eab5373fd08650406aa59ca6e5a1
Mozex <mozex.gm@gmail.com>
2024-05-01 13:43:21 +0300

    add messages streamed response message

---
+++ src/Responses/Messages/CreateStreamedResponseMessage.php
@@ -0,0 +1,51 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Anthropic\Responses\Messages;
+
+final class CreateStreamedResponseMessage
+{
+    private function __construct(
+        public readonly ?string $id,
+        public readonly ?string $type,
+        public readonly ?string $role,
+        public readonly ?array $content,
+        public readonly ?string $model,
+        public readonly ?string $stop_reason,
+        public readonly ?string $stop_sequence,
+    ) {
+    }
+
+    /**
+     * @param  array{id: string, type: string, role: string, content: array, model: string, stop_reason: string|null, stop_sequence:string|null}  $attributes
+     */
+    public static function from(array $attributes): self
+    {
+        return new self(
+            $attributes['id'] ?? null,
+            $attributes['type'] ?? null,
+            $attributes['role'] ?? null,
+            $attributes['content'] ?? null,
+            $attributes['model'] ?? null,
+            $attributes['stop_reason'] ?? null,
+            $attributes['stop_sequence'] ?? null,
+        );
+    }
+
+    /**
+     * @return array{id: string|null, type: string|null, role: string|null, content: array|null, model: string|null, stop_reason: string|null, stop_sequence:string|null}
+     */
+    public function toArray(): array
+    {
+        return [
+            'id' => $this->id,
+            'type' => $this->type,
+            'role' => $this->role,
+            'content' => $this->content,
+            'model' => $this->model,
+            'stop_reason' => $this->stop_reason,
+            'stop_sequence' => $this->stop_sequence,
+        ];
+    }
+}
