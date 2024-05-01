a2bbb611374b39f36d511a2d35c3730b19b9af3a
Mozex <mozex.gm@gmail.com>
2024-05-01 13:42:15 +0300

    fix meta information

--- src/Responses/Meta/MetaInformation.php
+++ src/Responses/Meta/MetaInformation.php
@@ -6,54 +6,46 @@ use Anthropic\Contracts\MetaInformationContract;
 use Anthropic\Responses\Concerns\ArrayAccessible;
 
 /**
- * @implements MetaInformationContract<array{x-request-id?: string, openai-model?: string, openai-organization?: string, openai-processing-ms?: int, openai-version?: string, x-ratelimit-limit-requests?: int, x-ratelimit-limit-tokens?: int, x-ratelimit-remaining-requests?: int, x-ratelimit-remaining-tokens?: int, x-ratelimit-reset-requests?: string, x-ratelimit-reset-tokens?: string}>
+ * @use ArrayAccessible<array{request-id?: string, anthropic-ratelimit-requests-limit?: int, anthropic-ratelimit-tokens-limit?: int, anthropic-ratelimit-requests-remaining?: int, anthropic-ratelimit-tokens-remaining?: int, anthropic-ratelimit-requests-reset?: string, anthropic-ratelimit-tokens-reset?: string}>
  */
 final class MetaInformation implements MetaInformationContract
 {
     /**
-     * @use ArrayAccessible<array{x-request-id?: string, openai-model?: string, openai-organization?: string, openai-processing-ms?: int, openai-version?: string, x-ratelimit-limit-requests?: int, x-ratelimit-limit-tokens?: int, x-ratelimit-remaining-requests?: int, x-ratelimit-remaining-tokens?: int, x-ratelimit-reset-requests?: string, x-ratelimit-reset-tokens?: string}>
+     * @use ArrayAccessible<array{request-id?: string, anthropic-ratelimit-requests-limit?: int, anthropic-ratelimit-tokens-limit?: int, anthropic-ratelimit-requests-remaining?: int, anthropic-ratelimit-tokens-remaining?: int, anthropic-ratelimit-requests-reset?: string, anthropic-ratelimit-tokens-reset?: string}>
      */
     use ArrayAccessible;
 
     private function __construct(
         public ?string $requestId,
-        public readonly MetaInformationAnthropic $anthropic,
         public readonly ?MetaInformationRateLimit $requestLimit,
         public readonly ?MetaInformationRateLimit $tokenLimit,
     ) {
     }
 
     /**
-     * @param  array{x-request-id: string[], openai-model: string[], openai-organization: string[], openai-version: string[], openai-processing-ms: string[], x-ratelimit-limit-requests: string[], x-ratelimit-remaining-requests: string[], x-ratelimit-reset-requests: string[], x-ratelimit-limit-tokens: string[], x-ratelimit-remaining-tokens: string[], x-ratelimit-reset-tokens: string[]}  $headers
+     * @param  array{request-id: string[], anthropic-ratelimit-requests-limit: string[], anthropic-ratelimit-requests-remaining: string[], anthropic-ratelimit-requests-reset: string[], anthropic-ratelimit-tokens-limit: string[], anthropic-ratelimit-tokens-remaining: string[], anthropic-ratelimit-tokens-reset: string[]}  $headers
      */
     public static function from(array $headers): self
     {
         $headers = array_change_key_case($headers, CASE_LOWER);
 
-        $requestId = $headers['x-request-id'][0] ?? null;
+        $requestId = $headers['request-id'][0] ?? null;
 
-        $anthropic = MetaInformationAnthropic::from([
-            'model' => $headers['openai-model'][0] ?? null,
-            'organization' => $headers['openai-organization'][0] ?? null,
-            'version' => $headers['openai-version'][0] ?? null,
-            'processingMs' => isset($headers['openai-processing-ms'][0]) ? (int) $headers['openai-processing-ms'][0] : null,
-        ]);
-
-        if (isset($headers['x-ratelimit-remaining-requests'][0])) {
+        if (isset($headers['anthropic-ratelimit-requests-remaining'][0])) {
             $requestLimit = MetaInformationRateLimit::from([
-                'limit' => isset($headers['x-ratelimit-limit-requests'][0]) ? (int) $headers['x-ratelimit-limit-requests'][0] : null,
-                'remaining' => (int) $headers['x-ratelimit-remaining-requests'][0],
-                'reset' => $headers['x-ratelimit-reset-requests'][0] ?? null,
+                'limit' => isset($headers['anthropic-ratelimit-requests-limit'][0]) ? (int) $headers['anthropic-ratelimit-requests-limit'][0] : null,
+                'remaining' => (int) $headers['anthropic-ratelimit-requests-remaining'][0],
+                'reset' => $headers['anthropic-ratelimit-requests-reset'][0] ?? null,
             ]);
         } else {
             $requestLimit = null;
         }
 
-        if (isset($headers['x-ratelimit-remaining-tokens'][0])) {
+        if (isset($headers['anthropic-ratelimit-tokens-remaining'][0])) {
             $tokenLimit = MetaInformationRateLimit::from([
-                'limit' => isset($headers['x-ratelimit-limit-tokens'][0]) ? (int) $headers['x-ratelimit-limit-tokens'][0] : null,
-                'remaining' => (int) $headers['x-ratelimit-remaining-tokens'][0],
-                'reset' => $headers['x-ratelimit-reset-tokens'][0] ?? null,
+                'limit' => isset($headers['anthropic-ratelimit-tokens-limit'][0]) ? (int) $headers['anthropic-ratelimit-tokens-limit'][0] : null,
+                'remaining' => (int) $headers['anthropic-ratelimit-tokens-remaining'][0],
+                'reset' => $headers['anthropic-ratelimit-tokens-reset'][0] ?? null,
             ]);
         } else {
             $tokenLimit = null;
@@ -61,7 +53,6 @@ final class MetaInformation implements MetaInformationContract
 
         return new self(
             $requestId,
-            $anthropic,
             $requestLimit,
             $tokenLimit,
         );
@@ -73,17 +64,13 @@ final class MetaInformation implements MetaInformationContract
     public function toArray(): array
     {
         return array_filter([
-            'openai-model' => $this->anthropic->model,
-            'openai-organization' => $this->anthropic->organization,
-            'openai-processing-ms' => $this->anthropic->processingMs,
-            'openai-version' => $this->anthropic->version,
-            'x-ratelimit-limit-requests' => $this->requestLimit->limit ?? null,
-            'x-ratelimit-limit-tokens' => $this->tokenLimit->limit ?? null,
-            'x-ratelimit-remaining-requests' => $this->requestLimit->remaining ?? null,
-            'x-ratelimit-remaining-tokens' => $this->tokenLimit->remaining ?? null,
-            'x-ratelimit-reset-requests' => $this->requestLimit->reset ?? null,
-            'x-ratelimit-reset-tokens' => $this->tokenLimit->reset ?? null,
-            'x-request-id' => $this->requestId,
+            'anthropic-ratelimit-requests-limit' => $this->requestLimit->limit ?? null,
+            'anthropic-ratelimit-tokens-limit' => $this->tokenLimit->limit ?? null,
+            'anthropic-ratelimit-requests-remaining' => $this->requestLimit->remaining ?? null,
+            'anthropic-ratelimit-tokens-remaining' => $this->tokenLimit->remaining ?? null,
+            'anthropic-ratelimit-requests-reset' => $this->requestLimit->reset ?? null,
+            'anthropic-ratelimit-tokens-reset' => $this->tokenLimit->reset ?? null,
+            'request-id' => $this->requestId,
         ], fn (string|int|null $value): bool => ! is_null($value));
     }
 }
