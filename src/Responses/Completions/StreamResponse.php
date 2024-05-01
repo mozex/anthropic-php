9183e4ee0178e8c0031f810a310eadb08cae700a
Mozex <mozex.gm@gmail.com>
2024-05-01 13:41:32 +0300

    add completion stream support

---
+++ src/Responses/Completions/StreamResponse.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace Anthropic\Responses\Completions;
+
+use Anthropic\Contracts\ResponseStreamContract;
+use Anthropic\Exceptions\ErrorException;
+use Generator;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\StreamInterface;
+
+/**
+ * @template TResponse
+ *
+ * @implements ResponseStreamContract<TResponse>
+ */
+final class StreamResponse implements ResponseStreamContract
+{
+    /**
+     * Creates a new Stream Response instance.
+     *
+     * @param  class-string<TResponse>  $responseClass
+     */
+    public function __construct(
+        private readonly string $responseClass,
+        private readonly ResponseInterface $response,
+    ) {
+        //
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getIterator(): Generator
+    {
+        while (! $this->response->getBody()->eof()) {
+            $line = $this->readLine($this->response->getBody());
+
+            if (! str_starts_with($line, 'data:')) {
+                continue;
+            }
+
+            $data = trim(substr($line, strlen('data:')));
+
+            /** @var array{error?: array{type: ?string, message: string|array<int, string>}} $response */
+            $response = json_decode($data, true, flags: JSON_THROW_ON_ERROR);
+
+            if (isset($response['error'])) {
+                throw new ErrorException($response['error']);
+            }
+
+            if (! isset($response['type'])) {
+                continue;
+            }
+
+            if ($response['type'] !== 'completion') {
+                continue;
+            }
+
+            yield $this->responseClass::from($response);
+        }
+    }
+
+    /**
+     * Read a line from the stream.
+     */
+    private function readLine(StreamInterface $stream): string
+    {
+        $buffer = '';
+
+        while (! $stream->eof()) {
+            if ('' === ($byte = $stream->read(1))) {
+                return $buffer;
+            }
+            $buffer .= $byte;
+            if ($byte === "\n") {
+                break;
+            }
+        }
+
+        return $buffer;
+    }
+}
