1169ddf4134278c47f08a020143e7af6356a42b2
Mozex <mozex.gm@gmail.com>
2024-05-01 13:44:48 +0300

    test for transporter base uri

--- tests/ValueObjects/Transporter/BaseUri.php
+++ tests/ValueObjects/Transporter/BaseUri.php
@@ -3,19 +3,19 @@
 use Anthropic\ValueObjects\Transporter\BaseUri;
 
 it('can be created from a string', function () {
-    $baseUri = BaseUri::from('api.openai.com/v1');
+    $baseUri = BaseUri::from('api.anthropic.com/v1');
 
-    expect($baseUri->toString())->toBe('https://api.openai.com/v1/');
+    expect($baseUri->toString())->toBe('https://api.anthropic.com/v1/');
 });
 
 it('can be created from a string with http protocol', function () {
-    $baseUri = BaseUri::from('http://api.openai.com/v1');
+    $baseUri = BaseUri::from('http://api.anthropic.com/v1');
 
-    expect($baseUri->toString())->toBe('http://api.openai.com/v1/');
+    expect($baseUri->toString())->toBe('http://api.anthropic.com/v1/');
 });
 
 it('can be created from a string with https protocol', function () {
-    $baseUri = BaseUri::from('https://api.openai.com/v1');
+    $baseUri = BaseUri::from('https://api.anthropic.com/v1');
 
-    expect($baseUri->toString())->toBe('https://api.openai.com/v1/');
+    expect($baseUri->toString())->toBe('https://api.anthropic.com/v1/');
 });
